e0abe1a09ed5fba552a10e0c5ca5a3816a51b97f
Zpiboo <zpiboo@outlook.com>
2024-10-27 15:51:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MPK Speedrun
+This is a module for [MPKMod 2](https://github.com/kurrycat2004/MPKMod_2), a Minecraft mod for parkour.
+It introduces many quality of life features to improve your parkour speedrunning experience.
---
+++ src/main/java/io/github/zpiboo/mpkspeedrun/MPKSpeedrun.java
@@ -0,0 +1,14 @@
+package io.github.zpiboo.mpkspeedrun;
+
+import io.github.kurrycat.mpkmod.modules.MPKModule;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MPKSpeedrun implements MPKModule {
+    public static final String MODULE_NAME = "mpkspeedrun";
+    public static final Logger LOGGER = LogManager.getLogger(MODULE_NAME);
+
+    public void init() {}
+
+    public void loaded() {}
+}
