b24d049de262718fcde1b812107693ef3a97d5f4
Zpiboo <93818768+Zpiboo@users.noreply.github.com>
2025-02-06 21:25:43 +0000

    add map save system

--- src/main/java/io/github/zpiboo/mpkspeedrun/parkourmaps/Map.java
+++ src/main/java/io/github/zpiboo/mpkspeedrun/parkourmaps/Map.java
@@ -1,7 +1,17 @@
 package io.github.zpiboo.mpkspeedrun.parkourmaps;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+
 import io.github.kurrycat.mpkmod.gui.infovars.InfoString;
 import io.github.kurrycat.mpkmod.util.BoundingBox3D;
+import io.github.zpiboo.mpkspeedrun.MPKSpeedrun;
+import io.github.zpiboo.mpkspeedrun.util.FileUtil;
 
 @InfoString.DataClass
 public class Map {
@@ -10,24 +20,30 @@ public class Map {
     private TriggerZone start;
     private TriggerZone finish;
 
-    @InfoString.Getter
-    public BoundingBox3D getStartBox() { return start.getZone(); }
-    @InfoString.Getter
-    public BoundingBox3D getFinishBox() { return finish.getZone(); }
-
     public Map(String name, TriggerZone start, TriggerZone finish) {
-        this.name = name;
-
-        this.start = start;
-        this.finish = finish;
+        setName(name);
+        setStart(start);
+        setFinish(finish);
     }
 
     @InfoString.Getter
-    public String getName() {
-        return name;
-    }
+    public BoundingBox3D getStartBox() { return start != null ? start.getZone() : null; }
+    @InfoString.Getter
+    public BoundingBox3D getFinishBox() { return finish != null ? finish.getZone() : null; }
+
+    @InfoString.Getter
+    public String getName() { return name; }
+
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null
+                ? getDefaultName()
+                : name;
+    }
+
+    public static String getDefaultName() {
+        File[] files = FileUtil.MAP_FOLDER.listFiles((dir, filename) -> filename.endsWith(".json"));
+        int num = files == null ? 1 : files.length + 1;
+        return "Map " + num;
     }
 
     public TriggerZone getStart() {
@@ -42,5 +58,58 @@ public class Map {
     }
     public void setFinish(TriggerZone finish) {
         this.finish = finish;
+    }
+
+    public JSONObject toJson() {
+        JSONObject mapJson = new JSONObject()
+            .put("start", start != null ? start.toJson() : new JSONObject())
+            .put("finish", finish != null ? finish.toJson() : new JSONObject());
+
+        return mapJson;
+    }
+    public static Map fromJson(JSONObject mapJson) {
+        String name = getDefaultName();
+
+        TriggerZone start = TriggerZone.fromJson( mapJson.optJSONObject("start") );
+        TriggerZone finish = TriggerZone.fromJson( mapJson.optJSONObject("finish") );
+
+        return new Map(name, start, finish);
+    }
+
+    public Path getFilePath() {
+        return Paths.get(FileUtil.MAP_FOLDER_PATH, name + ".json");
+    }
+    public static Path getFilePath(String mapName) {
+        return Paths.get(FileUtil.MAP_FOLDER_PATH, mapName + ".json");
+    }
+
+    public void save() {
+        Path filePath = getFilePath();
+        try {
+            Files.writeString(filePath, toJson().toString(2));
+        } catch (IOException e) {
+            MPKSpeedrun.LOGGER.error("Failed to create file: " + filePath + " - " + e.getMessage(), e);
+        }
+    }
+    public static Map load(String mapName) {
+        Path filePath = getFilePath(mapName);
+
+        if (!Files.exists(filePath)) {
+            MPKSpeedrun.LOGGER.warn("Couldn't find map file: " + filePath);
+            return null;
+        }
+
+        try {
+            Map pkMap = fromJson( new JSONObject(Files.readString(filePath)) );
+            pkMap.setName(mapName);
+
+            return pkMap;
+        } catch (IOException e) {
+            MPKSpeedrun.LOGGER.error("Failed to read map file: " + filePath + " - " + e.getMessage(), e);
+        } catch (Exception e) {
+            MPKSpeedrun.LOGGER.error("Invalid JSON format in map file: " + filePath + " - " + e.getMessage(), e);
+        }
+
+        return null;
     }
 }
--- src/main/java/io/github/zpiboo/mpkspeedrun/parkourmaps/TriggerZone.java
+++ src/main/java/io/github/zpiboo/mpkspeedrun/parkourmaps/TriggerZone.java
@@ -1,13 +1,18 @@
 package io.github.zpiboo.mpkspeedrun.parkourmaps;
 
+import org.json.JSONObject;
+
 import io.github.kurrycat.mpkmod.compatibility.MCClasses.Player;
 import io.github.kurrycat.mpkmod.util.BoundingBox3D;
 import io.github.kurrycat.mpkmod.util.Vector3D;
+import io.github.zpiboo.mpkspeedrun.MPKSpeedrun;
 import io.github.zpiboo.mpkspeedrun.util.BB3D;
 
 public class TriggerZone {
     private BoundingBox3D zone;
     private TriggerMode mode;
+
+    public static TriggerZone ZERO = new TriggerZone();
 
     public enum TriggerMode {
         POS_ENTER,
@@ -17,8 +22,18 @@ public class TriggerZone {
     }
 
     public TriggerZone(BoundingBox3D zone, TriggerMode mode) {
-        this.zone = zone;
+        this.zone = new BoundingBox3D(zone.getMin(), zone.getMax());
         this.mode = mode;
+    }
+    public TriggerZone() {
+        this(BoundingBox3D.ZERO, TriggerMode.POS_ENTER);
+    }
+
+    public BoundingBox3D getZone() {
+        return zone;
+    }
+    public void setZone(BoundingBox3D zone) {
+        this.zone = zone;
     }
 
     public boolean shouldTrigger(Player player) {
@@ -42,5 +57,44 @@ public class TriggerZone {
 
             default: return false;
         }
+    }
+
+    public JSONObject toJson() {
+        JSONObject zoneJson = new JSONObject()
+            .put("mode", mode.toString())
+
+            .put("minx", zone.minX())
+            .put("miny", zone.minY())
+            .put("minz", zone.minZ())
+
+            .put("maxx", zone.maxX())
+            .put("maxy", zone.maxY())
+            .put("maxz", zone.maxZ());
+
+        return zoneJson;
+    }
+    public static TriggerZone fromJson(JSONObject zoneJson) {
+        String modeString = zoneJson.optString("mode", "POS_ENTER");
+        TriggerMode mode;
+        try {
+            mode = TriggerMode.valueOf(modeString);
+        } catch (IllegalArgumentException e) {
+            mode = TriggerMode.POS_ENTER;
+            MPKSpeedrun.LOGGER.warn("Trigger mode not found: " + modeString);
+        }
+
+        double minX = zoneJson.optDouble("minx", 0);
+        double minY = zoneJson.optDouble("miny", 0);
+        double minZ = zoneJson.optDouble("minz", 0);
+        double maxX = zoneJson.optDouble("maxx", 0);
+        double maxY = zoneJson.optDouble("maxy", 0);
+        double maxZ = zoneJson.optDouble("maxz", 0);
+
+        BoundingBox3D zone = new BoundingBox3D(
+                new Vector3D(minX, minY, minZ),
+                new Vector3D(maxX, maxY, maxZ)
+        );
+
+        return new TriggerZone(zone, mode);
     }
 }
