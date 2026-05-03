921902cceb9827dd1d98e14d969164c065f1d276
Zpiboo <zpiboo@outlook.com>
2026-05-03 12:46:31 +0200

    feat: make subtick calculations only horizontal (xz plane)

--- src/main/java/io/github/zpiboo/mpkspeedrun/util/api_compat/BoundingBox3DUtil.java
+++ src/main/java/io/github/zpiboo/mpkspeedrun/util/api_compat/BoundingBox3DUtil.java
@@ -1,5 +1,6 @@
 package io.github.zpiboo.mpkspeedrun.util.api_compat;
 
+import io.github.kurrycat.mpkmod.util.BoundingBox2D;
 import io.github.kurrycat.mpkmod.util.BoundingBox3D;
 import io.github.kurrycat.mpkmod.util.Vector3D;
 
@@ -21,42 +22,45 @@ public class BoundingBox3DUtil {
                 bb1.minZ() <= bb2.maxZ();
     }
 
-    public static double slabMethod(Vector3D start, Vector3D end, BoundingBox3D box) {
-        double[] startArr = new double[3];
-        double[] endArr = new double[3];
-        double[] direction = new double[3];
-        double[] boxMin = new double[3];
-        double[] boxMax = new double[3];
+    public static double xzSlabMethod(Vector3D start, Vector3D end, BoundingBox3D box) {
+        // Start pos is above or below
+        if (box.minX() < start.getX() && start.getX() < box.maxX() &&
+                box.minZ() < start.getZ() && start.getZ() < box.maxZ() &&
+                (start.getY() < box.minY() || box.maxY() < start.getY())) {
+            return 0.0;
+        }
+
+        double[] startArr = new double[2];
+        double[] endArr = new double[2];
+        double[] direction = new double[2];
+        double[] boxMin = new double[2];
+        double[] boxMax = new double[2];
 
         startArr[0] = start.getX();
-        startArr[1] = start.getY();
-        startArr[2] = start.getZ();
+        startArr[1] = start.getZ();
         endArr[0] = end.getX();
-        endArr[1] = end.getY();
-        endArr[2] = end.getZ();
+        endArr[1] = end.getZ();
 
         boxMin[0] = box.minX();
-        boxMin[1] = box.minY();
-        boxMin[2] = box.minZ();
+        boxMin[1] = box.minZ();
         boxMax[0] = box.maxX();
-        boxMax[1] = box.maxY();
-        boxMax[2] = box.maxZ();
+        boxMax[1] = box.maxZ();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             direction[i] = endArr[i] - startArr[i];
         }
 
         double tMin = 0.0;
         double tMax = 1.0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             double boxMinCoord = boxMin[i];
             double boxMaxCoord = boxMax[i];
 
             if (Math.abs(direction[i]) == 0) {
                 // Line is parallel to edge
                 if (startArr[i] < boxMinCoord || startArr[i] > boxMaxCoord) {
-                    return 1.0F;
+                    return 1.0;
                 }
             } else {
                 double t1 = (boxMinCoord - startArr[i]) / direction[i];
@@ -71,12 +75,12 @@ public class BoundingBox3DUtil {
                 tMin = Math.max(tMin, t1);
                 tMax = Math.min(tMax, t2);
 
-                if (tMin > tMax) { return 1.0F; }
+                if (tMin > tMax) { return 1.0; }
             }
         }
 
         if (tMax < 0 || tMin > 1) {
-            return 1.0F;
+            return 1.0;
         }
 
         return Math.max(tMin, 0.0);
